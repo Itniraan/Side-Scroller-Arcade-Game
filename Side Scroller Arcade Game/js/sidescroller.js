48b4c5e534c038c170d032cd03e008176c612c23
Itniraan <blakemurdo@gmail.com>
2014-11-05 14:48:31 -0500

    Added basic structure for init, queue, gameloop functions

--- Side Scroller Arcade Game/js/sidescroller.js
+++ Side Scroller Arcade Game/js/sidescroller.js
@@ -1,1 +1,28 @@
-﻿//# sourceMappingURL=sidescroller.js.map
+﻿var stage;
+var game;
+var queue;
+
+function loadQueue() {
+    queue = new createjs.LoadQueue();
+    queue.installPlugin(createjs.Sound);
+    createjs.Sound.alternateExtensions = ["mp3"];
+    queue.addEventListener("complete", init);
+
+    queue.loadManifest([]);
+}
+;
+
+// Initalization function, this sets up the stage and ticker
+function init() {
+    stage = new createjs.Stage(document.getElementById("gameCanvas"));
+    stage.enableMouseOver(20);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    createjs.Ticker.setFPS(60);
+}
+;
+
+function gameLoop() {
+    stage.update();
+}
+;
+//# sourceMappingURL=sidescroller.js.map
--- Side Scroller Arcade Game/js/sidescroller.ts
+++ Side Scroller Arcade Game/js/sidescroller.ts
@@ -1,1 +1,25 @@
-﻿
+﻿var stage: createjs.Stage;
+var game: createjs.Container;
+var queue: createjs.LoadQueue;
+
+function loadQueue() {
+    queue = new createjs.LoadQueue();
+    queue.installPlugin(createjs.Sound);
+    createjs.Sound.alternateExtensions = ["mp3"];
+    queue.addEventListener("complete", init);
+
+    queue.loadManifest([
+    ]);
+};
+
+// Initalization function, this sets up the stage and ticker
+function init() {
+    stage = new createjs.Stage(document.getElementById("gameCanvas"));
+    stage.enableMouseOver(20);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    createjs.Ticker.setFPS(60);
+};
+
+function gameLoop() {
+    stage.update();
+};
