5863a170ff219d884659a7138f593805d12ccf59
Itniraan <blakemurdo@gmail.com>
2014-11-11 19:51:48 -0500

    Add JS

---
+++ Side Scroller Arcade Game/js/objects/crystal.js
@@ -0,0 +1,37 @@
+﻿var objects;
+(function (objects) {
+    // Crystal Class
+    var Crystal = (function () {
+        function Crystal(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Bitmap(queue.getResult("crystal"));
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.image.regX = this.width * 0.5;
+            this.image.regY = this.height * 0.5;
+            this.dy = 5;
+
+            game.addChild(this.image);
+            this.reset();
+        }
+        Crystal.prototype.reset = function () {
+            this.image.x = 630;
+            this.image.y = Math.floor(Math.random() * stage.canvas.height);
+        };
+
+        Crystal.prototype.update = function () {
+            this.image.x -= this.dy;
+            if (this.image.x < 0) {
+                this.reset();
+            }
+        };
+
+        Crystal.prototype.destroy = function () {
+            game.removeChild(this.image);
+        };
+        return Crystal;
+    })();
+    objects.Crystal = Crystal;
+})(objects || (objects = {}));
+//# sourceMappingURL=crystal.js.map
---
+++ Side Scroller Arcade Game/js/objects/star.js
@@ -0,0 +1,37 @@
+﻿var objects;
+(function (objects) {
+    // Star Class
+    var Star = (function () {
+        function Star(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Bitmap(queue.getResult("star"));
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.image.regX = this.width * 0.5;
+            this.image.regY = this.height * 0.5;
+            this.dy = 5;
+
+            game.addChild(this.image);
+            this.reset();
+        }
+        Star.prototype.reset = function () {
+            this.image.x = 630;
+            this.image.y = Math.floor(Math.random() * stage.canvas.height);
+        };
+
+        Star.prototype.update = function () {
+            this.image.x -= this.dy;
+            if (this.image.x < 0) {
+                this.reset();
+            }
+        };
+
+        Star.prototype.destroy = function () {
+            game.removeChild(this.image);
+        };
+        return Star;
+    })();
+    objects.Star = Star;
+})(objects || (objects = {}));
+//# sourceMappingURL=star.js.map
